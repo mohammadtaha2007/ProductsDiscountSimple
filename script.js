86a588adb186e871a23e00daa47aec4c886e8012
MohammadTaha Rahimi <mohammadtahar2007@gmail.com>
2026-01-08 13:24:08 +0330

    CreateObject

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // Create Product Objects
+const products = [
+    {name: "Laptop", price: 1200, inStock: true, discount: 10},
+    {name: "Phone", price: 800, inStock: false, discount: 5},
+    {name: "Headphones", price: 150, inStock: true, discount: 20},
+    {name: "Mouse", price: 50, inStock: true, discount: 0},
+];
